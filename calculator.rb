a68e9a6349bd81d51a796c2bf469cfb262718a2c
munificent-forum-1695 <bunchshawn@gmail.com>
2020-10-07 17:04:22 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,3 @@
-require_relative
 
 first_number = 10
 second_number = 2
@@ -6,4 +5,4 @@ second_number = 2
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
-quotient = first_number ? second_number
+quotient = first_number / second_number
